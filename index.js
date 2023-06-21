849796510752557c2fd8df4ef50a2dc5340c33bf
kuanchiliao1 <kuanchiliao@gmail.com>
2023-06-21 16:57:58 -0700

    Wrote append function

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-const LinkedListFactory = () => {
+const LinkedListFactory = function() {
+  let head = NodeFactory(null, null)
 
+  const append = (value) => {
+    if (head.nextNode === null) {
+      head.nextNode = NodeFactory(value)
+    } else {
+      let next = head.nextNode
+      while (next.nextNode !== null) {
+        next = next.nextNode
+      }
+      next.nextNode = NodeFactory(value)
+    }
+
+    // if the head's nextNode is null, then
+    // we set its nextNode property
+    // if head's nextNode is NOT null, then
+    // we go to the next node until we find
+    // something that is null
+    // next = obj.next1
+    // next = next.next1
+  }
+
+  return {
+    append,
+    head
+  }
 }
 
 const NodeFactory = (value = null, nextNode = null) => {
@@ -8,3 +33,11 @@ const NodeFactory = (value = null, nextNode = null) => {
     nextNode
   }
 }
+
+const list = LinkedListFactory()
+list.append(200)
+list.append(300)
+list.append(400)
+
+
+console.log(list.head)
